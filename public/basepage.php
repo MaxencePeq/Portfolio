49439001b32fe3feadd42f9343e890ccaff28e14
Pequeno Maxence <maxence.pequeno@etudiant.univ-reims.fr>
2025-11-24 14:24:57 +0100

    Ajout de la navbar et de son css

--- public/basepage.php
+++ public/basepage.php
@@ -7,4 +7,21 @@ $webpage->setTitle("Maxence Portfolio");
 
 $webpage->appendCssUrl("css/off_portfolio.css");
 
+$webpage->appendContent(<<<HTML
+<nav class="navbar">
+        <a class="navbar-item" href="https://github.com/MaxencePeq"><img src="img/githubIcon.png" width="40" height="40" alt="GitHub"></a>
+        <a class="navbar-item" href="cv.php"><img src="img/cvlogo.png" width="40" height="40" alt="CV"></a>
+        <a class="navbar-item" href="https://www.instagram.com/pianomaxence/"><img src="img/instagram.png" width="40" height="40" alt="Instagram"></a>
+        <a class="navbar-item" href="https://www.linkedin.com/in/maxence-pequeno-671581329/"><img src="img/linkedin.png" width="40" height="40" alt="LinkedIn"></a>
+    </nav>
+HTML);  // <- Nav-bar
+
+$webpage->appendContent(<<<HTML
+<div class="card">
+
+</div>
+
+HTML);
+
+
 echo($webpage->toHtml());
