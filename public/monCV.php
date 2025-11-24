0f90b63966996e1da30674fd38285e331d352f89
Pequeno Maxence <maxence.pequeno@etudiant.univ-reims.fr>
2025-11-24 17:31:47 +0100

    Ajout de l'image, des boutons et du texte

--- public/monCV.php
+++ public/monCV.php
@@ -4,7 +4,7 @@ use Maxence\OfficialGitHubPortfolio\html\WebPage;
 
 $webpage = new WebPage();
 $webpage->setTitle("Maxence CV");
-$webpage->appendCssUrl("css/cv.css");
+$webpage->appendCssUrl("css/off_portfolio.css");
 
 $webpage->appendContent(<<<HTML
 <nav class="navbar">
@@ -14,5 +14,25 @@ $webpage->appendContent(<<<HTML
         <a class="navbar-item" href="https://www.linkedin.com/in/maxence-pequeno-671581329/"><img src="img/linkedin.png" width="40" height="40" alt="LinkedIn"></a>
     </nav>
 HTML); // <- Nav-bar
+
+$webpage->appendContent(<<<HTML
+<div class="card">
+    <div class="card-body">
+        <h1 class="titre2">Mon CV</h1>
+        <p class="txt1">Aperçu en pleine largeur, avec options de téléchargement et d’impression.</p>
+        
+        <div class="cv">
+            <img class="cv-img" src="img/CVmaxpeq.png" alt="CV de Maxence Pequeno" width="707" height="1000">
+        </div>
+        
+        <div class="cv-actions">
+            <a class="btn1" href="img/CVmaxpeq.png" download>Télécharger</a>
+            <button class="btn2" type="button" onclick="window.print()">Imprimer</button>
+            <a class="btn2" href="playpage.php">← Retour</a>
+        </div>
+        
+    </div>
+</div>
+HTML);  //
 
 echo $webpage->toHtml();
