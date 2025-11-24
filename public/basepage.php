4bc99dd19ff9ce7d1a791857fd4646f504b32e2b
Pequeno Maxence <maxence.pequeno@etudiant.univ-reims.fr>
2025-11-24 15:49:53 +0100

    Création de la page du CV

--- public/basepage.php
+++ public/basepage.php
@@ -10,7 +10,7 @@ $webpage->appendCssUrl("css/off_portfolio.css");
 $webpage->appendContent(<<<HTML
 <nav class="navbar">
         <a class="navbar-item" href="https://github.com/MaxencePeq"><img src="img/githubIcon.png" width="40" height="40" alt="GitHub"></a>
-        <a class="navbar-item" href="cv.php"><img src="img/cvlogo.png" width="40" height="40" alt="CV"></a>
+        <a class="navbar-item" href="monCV.php"><img src="img/cvlogo.png" width="40" height="40" alt="CV"></a>
         <a class="navbar-item" href="https://www.instagram.com/pianomaxence/"><img src="img/instagram.png" width="40" height="40" alt="Instagram"></a>
         <a class="navbar-item" href="https://www.linkedin.com/in/maxence-pequeno-671581329/"><img src="img/linkedin.png" width="40" height="40" alt="LinkedIn"></a>
     </nav>
---
+++ public/monCV.php
@@ -0,0 +1,7 @@
+<?php
+
+use Maxence\OfficialGitHubPortfolio\html\WebPage;
+
+$webpage = new WebPage();
+$webpage->setTitle("Maxence CV");
+$webpage->appendCssUrl("css/cv.css");
