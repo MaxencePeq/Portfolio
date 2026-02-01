c7bda7fd0311c527a164016c85495276bb5b42a7
Pequeno Maxence <maxence.pequeno@etudiant.univ-reims.fr>
2026-02-01 17:23:22 +0100

    feat (js) : Ajout du lien de redirection vers le site de l'image

--- public/script/carrousel.js
+++ public/script/carrousel.js
@@ -1,38 +1,62 @@
 const collections = {
-    LebelOuvrage : [
-        "img/LebelOuvrage/accueil.png",
-        "img/LebelOuvrage/1.png",
-        "img/LebelOuvrage/2.png",
-        "img/LebelOuvrage/3.png"
-    ],
-    BuddySystem : [
-        "img/buddysystem/accueil.png",
-        "img/buddysystem/chat.png",
-        "img/buddysystem/login.png",
-        "img/buddysystem/etudiant.png",
-        "img/buddysystem/profile.png",
-        "img/buddysystem/famille.png"
-    ],
-    BakerySimulator : [
-        "img/BakerySim-img/intro.png",
-        "img/BakerySim-img/full.png",
-        "img/BakerySim-img/debug.png"
-    ],
-    WebMusic : [
-        "img/webmusic-img/index.png",
-        "img/webmusic-img/album.png",
-        "img/webmusic-img/album-track.png",
-        "img/webmusic-img/song.png"
-    ]
+    LebelOuvrage : {
+        images: [
+            "img/LebelOuvrage/accueil.png",
+            "img/LebelOuvrage/1.png",
+            "img/LebelOuvrage/2.png",
+            "img/LebelOuvrage/3.png"
+        ],
+        lien: "https://lebelouvrage.bzh"
+    },
+    BuddySystem : {
+        images: [
+            "img/buddysystem/accueil.png",
+            "img/buddysystem/chat.png",
+            "img/buddysystem/login.png",
+            "img/buddysystem/etudiant.png",
+            "img/buddysystem/profile.png",
+            "img/buddysystem/famille.png"
+        ],
+        lien: null
+    },
+    BakerySimulator : {
+        images: [
+            "img/BakerySim-img/intro.png",
+            "img/BakerySim-img/full.png",
+            "img/BakerySim-img/debug.png"
+        ],
+        lien: "https://bakerysimulator.alwaysdata.net/BakerySimulator/public/index.php"
+    },
+    WebMusic : {
+        images: [
+            "img/webmusic-img/index.png",
+            "img/webmusic-img/album.png",
+            "img/webmusic-img/album-track.png",
+            "img/webmusic-img/song.png"
+        ],
+        lien: null
+    }
 }
 
 let collectionActuelle = [];
+let lienActuel = null;
 let position = 0;
 
 function ouvrirCarrousel(nom){
-    collectionActuelle = collections[nom];
+    collectionActuelle = collections[nom].images;
+    lienActuel = collections[nom].lien;
     position = 0;
-    document.getElementById('image').src = collectionActuelle[position];
+
+    const imageElement = document.getElementById('image');
+    imageElement.src = collectionActuelle[position];
+
+    if (lienActuel) {
+        imageElement.style.cursor = 'pointer';
+        imageElement.onclick = () => window.open(lienActuel, '_blank');
+    } else {
+        imageElement.style.cursor = 'default';
+        imageElement.onclick = null;
+    }
     document.getElementById('carrousel').style.display = 'flex';
 }
 
