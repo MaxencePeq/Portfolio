3c3d2f2e6d97428530fe217db920331ff07bf5c1
Pequeno Maxence <maxence.pequeno@etudiant.univ-reims.fr>
2026-03-09 18:51:27 +0100

    feat : re-factorisation du paragraphe de la partie "passion/ autres" + ajout du css au texte

--- public/basepage.php
+++ public/basepage.php
@@ -141,7 +141,6 @@ $webpage->appendContent(<<<HTML
             <li><a href="#LebelOuvrage"><img src="img/LanguagesEtOutils/git.png" loading="lazy" width="64" height="64" alt="Git"><span>Git</span></a></li>
             <li><img src="img/LanguagesEtOutils/phpmyadmin.png" loading="lazy" width="64" height="64" alt="phpMyAdmin"><span>phpMyAdmin</span></li>
             <li><a href="#BakerySimulator"><img src="img/LanguagesEtOutils/COMPOSER.png" loading="lazy" width="64" height="64" alt="Composer"><span>Composer</span></a></li>
-            <li><img src="img/LanguagesEtOutils/npm.png" loading="lazy" width="64" height="64" alt="npm"><span>npm</span></li>
         </ul>
 
         <h3 class="sous-titre">Langages</h3>
@@ -369,52 +368,52 @@ $webpage->appendContent(<<<HTML
     
         <div class="title">
             <h2> Autres </h2> 
-            <p> Au dela de ma passion évidente pour le développement informatique, je cultive depuis 6ans maintenant mon amour pour la musique et plus particulièrement pour le piano. 
-             <br>
-             Mais j'aime aussi tous ce qui touche à l'art, que ce soit la lecture avec ma passion dévorante pour les classiques francais et l'horreur fantastique, mais aussi pour la peinture aquarelle.
-             <br>
-             Pour les intéressées, voici une petite listes de mes lecture favorites :
-             </p>
+        </div>
+        
+        <div class="autres-intro">
+            <p>Au-delà de ma passion évidente pour le développement informatique, je cultive depuis 6 ans mon amour pour la musique et plus particulièrement pour le piano. </p>
+            <p>J'aime aussi tout ce qui touche à l'art : la lecture avec ma passion dévorante pour les classiques français et l'horreur fantastique, mais aussi la peinture aquarelle. </p>
+            <p>Pour les intéressé·es, voici une petite sélection de mes lectures favorites :</p>
+        </div>
              
-             <div class="livre-list">
-                <div class="livre-item">
-                    <img src="img/livres/bojangles.jpg" loading="lazy" alt="livres En attendant bojangles ">
-                    <span class="livre-titre">En attendant bojangles</span>
-                </div>
-                <div class="livre-item">
-                    <img src="img/livres/lmdf.jpg" loading="lazy" alt="livres La maison des feuilles ">
-                    <span class="livre-titre">La Maison des Feuilles</span>
-                </div>
-                <div class="livre-item">
-                    <img src="img/livres/wager.jpg" loading="lazy" alt="livres Les naufragées du Wager">
-                    <span class="livre-titre">Les naufragées du Wager</span>
-                </div>
-                
-                <div class="livre-item">
-                    <img src="img/livres/lotr.jpg" loading="lazy" alt="livres Le seigneur des anneaux">
-                    <span class="livre-titre">Le seigneur des anneaux</span>
-                </div>
-                
-                <div class="livre-item">
-                    <img src="img/livres/witcher.jpg" loading="lazy" alt="livres the witcher ">
-                    <span class="livre-titre">The witcher</span>
-                </div>
-                
-                <div class="livre-item">
-                    <img src="img/livres/petitprince.jpg" loading="lazy" alt="livres Le petit prince">
-                    <span class="livre-titre">Le petit prince</span>
-                </div>
-                
-                <div class="livre-item">
-                    <img src="img/livres/foret.jpg" loading="lazy" alt="livres L'appel de la fôret">
-                    <span class="livre-titre">L'appel de la fôret</span>
-                </div>
-                
-                <div class="livre-item">
-                    <img src="img/livres/gary.jpg" loading="lazy" alt="livres La promesse de l'aube ">
-                    <span class="livre-titre">La promesse de l'aube</span>
-                </div>
-             </div>
+         <div class="livre-list">
+            <div class="livre-item">
+                <img src="img/livres/bojangles.jpg" loading="lazy" alt="livres En attendant bojangles ">
+                <span class="livre-titre">En attendant bojangles</span>
+            </div>
+            <div class="livre-item">
+                <img src="img/livres/lmdf.jpg" loading="lazy" alt="livres La maison des feuilles ">
+                <span class="livre-titre">La Maison des Feuilles</span>
+            </div>
+            <div class="livre-item">
+                <img src="img/livres/wager.jpg" loading="lazy" alt="livres Les naufragées du Wager">
+                <span class="livre-titre">Les naufragées du Wager</span>
+            </div>
+            
+            <div class="livre-item">
+                <img src="img/livres/lotr.jpg" loading="lazy" alt="livres Le seigneur des anneaux">
+                <span class="livre-titre">Le seigneur des anneaux</span>
+            </div>
+            
+            <div class="livre-item">
+                <img src="img/livres/witcher.jpg" loading="lazy" alt="livres the witcher ">
+                <span class="livre-titre">The witcher</span>
+            </div>
+            
+            <div class="livre-item">
+                <img src="img/livres/petitprince.jpg" loading="lazy" alt="livres Le petit prince">
+                <span class="livre-titre">Le petit prince</span>
+            </div>
+            
+            <div class="livre-item">
+                <img src="img/livres/foret.jpg" loading="lazy" alt="livres L'appel de la fôret">
+                <span class="livre-titre">L'appel de la fôret</span>
+            </div>
+            
+            <div class="livre-item">
+                <img src="img/livres/gary.jpg" loading="lazy" alt="livres La promesse de l'aube ">
+                <span class="livre-titre">La promesse de l'aube</span>
+            </div>
         </div>
     </div>
 </div>
