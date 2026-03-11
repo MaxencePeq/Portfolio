14a4728a807019310f1d53180ebbb5777042f805
Pequeno Maxence <maxence.pequeno@etudiant.univ-reims.fr>
2026-03-11 11:11:48 +0100

    fix : update de la page des projets "incoming"

--- public/redirected-page/Incoming.php
+++ public/redirected-page/Incoming.php
@@ -7,17 +7,33 @@ $webpage = new WebPage();
 $webpage->setTitle("Mes projets Symfony");
 
 $webpage->appendCssUrl("../css/off_portfolio.css");
+$webpage->appendCssUrl("../css/card-templates.css");
+$webpage->appendCssUrl("../css/navbar.css");
+
+
 $webpage->appendToHead(<<<HTML
 <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 HTML);
 
 $webpage->appendContent(<<<HTML
 <nav class="navbar">
-        <a class="navbar-item" href="https://github.com/MaxencePeq"><img src="../img/githubIcon.png" width="40" height="40" alt="GitHub"></a>
-        <a class="navbar-item" href="../monCV.php"><img src="../img/cvlogo.png" width="40" height="40" alt="CV"></a>
-        <a class="navbar-item" href="https://www.instagram.com/pianomaxence/"><img src="../img/instagram.png" width="40" height="40" alt="Instagram"></a>
-        <a class="navbar-item" href="https://www.linkedin.com/in/maxence-pequeno-671581329/"><img src="../img/linkedin.png" width="40" height="40" alt="LinkedIn"></a>
-    </nav>
+    <span class="brand">MP</span>
+
+    <div class="navbar-links">
+        <a class="navbar-item" href="#home">Accueil</a>
+        <a class="navbar-item" href="#projects">Projets</a>
+        <a class="navbar-item" href="autres.php">Passions et autres</a>
+        <a class="navbar-item" href="monCV.php">CV</a>
+    </div>
+
+    <div class="navbar-socials">
+        <a class="navbar-icon" href="https://github.com/MaxencePeq" target="_blank"><img src="img/githubIcon.png" width="20" height="20" alt="GitHub"></a>
+        <a class="navbar-icon" href="https://www.instagram.com/pianomaxence/" target="_blank"><img src="img/instagram.png" width="20" height="20" alt="Instagram"></a>
+        <a class="navbar-icon" href="https://www.linkedin.com/in/maxence-pequeno-671581329/" target="_blank"><img src="img/linkedin.png" width="20" height="20" alt="LinkedIn"></a>
+        <button class="fa-regular fa-moon" id="theme-toggle" aria-label="Basculer le mode sombre"></button>
+    </div>
+</nav>
+
 HTML);  // <- Nav-bar
 
 $webpage->appendContent(<<<HTML
