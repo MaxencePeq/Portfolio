6ab33dcedf5c2d354936c0dfce45e1bfdbcdb178
MaxencePeq <maxence.pequeno@icloud.com>
2026-05-09 19:14:46 +0200

    Feat : implémentation de la navbar dans l'app.tsx

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+import Navbar from "./component/navbar";
+
 function App() {
+  const [darkmode, setDarkmode] = useState(false);
   return (
     <>
+      <Navbar darkmode={darkmode} setDarkmode={setDarkmode} />
       <p className="text-4xl text-blue-500">Portfolio coming soon...</p>
     </>
   );
