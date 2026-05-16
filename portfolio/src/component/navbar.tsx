22a9d7df4dde4548f417dbd80f8d25ec44105af0
MaxencePeq <maxence.pequeno@icloud.com>
2026-05-17 00:47:34 +0200

    Feat : Ajout du responsive sur la navbar.

--- portfolio/src/component/navbar.tsx
+++ portfolio/src/component/navbar.tsx
@@ -4,69 +4,63 @@ type navbarProps = {
 };
 
 export default function Navbar({ darkmode, setDarkmode }: navbarProps) {
-  let bgColor = "bg-[#FEFEFE]";
-  let titleColor = "text-[#2563EB]";
-  let textColor = "text-gray-600";
-
-  if (darkmode) {
-    bgColor = "bg-[#2D2D44]";
-    titleColor = "text-[#2563EB]";
-    textColor = "text-gray-600";
-  }
+  const bgColor = darkmode ? "bg-[#2D2D44]" : "bg-[#FEFEFE]";
+  const titleColor = "text-[#2563EB]";
+  const textColor = "text-gray-600";
 
   const hover = darkmode
-    ? "hover:rounded-lg hover:bg-[#424257] hover:text-gray-800 p-2"
-    : "hover:rounded-lg hover:bg-gray-100 hover:text-gray-900 p-2";
+    ? "hover:rounded-lg hover:bg-[#424257] hover:text-gray-800 p-1 sm:p-2"
+    : "hover:rounded-lg hover:bg-gray-100 hover:text-gray-900 p-1 sm:p-2";
 
   return (
     <div
-      className={`flex flex-row ${bgColor} p-3 px-6 justify-between items-center shadow-lg`}
+      className={`flex flex-row ${bgColor} p-2 sm:p-3 px-3 sm:px-6 justify-between items-center shadow-lg gap-2`}
     >
-      {/* Liens vers les différentes sections du site */}
-      <p className={`${titleColor} font-bold text-xl`}>MP</p>
-      <div className={`${textColor} text-sm flex flex-row gap-6`}>
-        <a href="" className={`${hover}`}>
+      <p className={`${titleColor} font-bold text-lg sm:text-xl`}>MP</p>
+
+      <div
+        className={`${textColor} text-xs sm:text-sm flex flex-row gap-1 sm:gap-6`}
+      >
+        <a href="" className={hover}>
           Accueil
         </a>
-        <a href="" className={`${hover}`}>
+        <a href="" className={hover}>
           Réalisations
         </a>
-        <a href="" className={`${hover}`}>
+        <a href="" className={hover}>
           Autres
         </a>
-        <a href="" className={`${hover}`}>
+        <a href="" className={hover}>
           CV
         </a>
       </div>
 
-      {/* logos */}
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-2 sm:gap-4 items-center">
         <a href="https://github.com/MaxencePeq">
           <img
             src="/img/navbar/githubIcon.png"
             alt="GitHub"
-            className="w-6 h-6"
+            className="w-5 h-5 sm:w-6 sm:h-6"
           />
         </a>
-
         <a href="https://www.linkedin.com/in/maxence-pequeno-671581329/">
           <img
             src="/img/navbar/linkedin.png"
             alt="linkedin"
-            className="w-6 h-6"
+            className="w-5 h-5 sm:w-6 sm:h-6"
           />
         </a>
         <a href="https://www.instagram.com/pianomaxence/">
           <img
             src="/img/navbar/instagram.png"
             alt="instagram"
-            className="w-6 h-6"
+            className="w-5 h-5 sm:w-6 sm:h-6"
           />
         </a>
         <img
           src="/img/navbar/darkmode.png"
           alt="darkmode"
-          className="w-6 h-6 cursor-pointer"
+          className="w-5 h-5 sm:w-6 sm:h-6 cursor-pointer"
           onClick={() => setDarkmode(!darkmode)}
         />
       </div>
