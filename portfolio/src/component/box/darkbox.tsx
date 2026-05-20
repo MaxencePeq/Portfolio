f3fc9003a3ef995ae54ea91d7eb83ad66b9bc0a1
MaxencePeq <maxence.pequeno@icloud.com>
2026-05-20 13:38:02 +0200

    Feat : le Number de la darbox est désormais nullable

--- portfolio/src/component/box/darkbox.tsx
+++ portfolio/src/component/box/darkbox.tsx
@@ -1,7 +1,7 @@
 type DarkboxProps = {
   darkmode: boolean;
   content: React.ReactNode;
-  number: number;
+  number?: number;
 };
 
 export default function Darkbox({ darkmode, content, number }: DarkboxProps) {
@@ -22,11 +22,13 @@ export default function Darkbox({ darkmode, content, number }: DarkboxProps) {
     <div
       className={`${bgColor} relative flex-wrap shadow-sm p-[3rem] border-1 ${borderColor} rounded-2xl ${responsiveClasses} ${hover}`}
     >
-      <span
-        className={`absolute top-3 right-3 flex h-8 w-8 items-center justify-center rounded-full bg-transparent border-2 ${numberBorderColor} text-sm font-bold text-white`}
-      >
-        {number}
-      </span>
+      {number !== undefined && (
+        <span
+          className={`absolute top-3 right-3 flex h-8 w-8 items-center justify-center rounded-full bg-transparent border-2 ${numberBorderColor} text-sm font-bold text-white`}
+        >
+          {number}
+        </span>
+      )}
 
       {content}
     </div>
